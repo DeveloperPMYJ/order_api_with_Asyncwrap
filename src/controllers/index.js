28fb2e0016ca5d2fa257a669d0e0e4d240281947
신유진 <julia011097@gmail.com>
2023-10-24 00:47:47 +0900

    branch 이동

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,5 @@
-const productController = require('./productController');
-const userController = require('./userController');
 const orderController = require('./orderController');
 
 module.exports = {
-  userController,
-  productController,
   orderController,
 };
